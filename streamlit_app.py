b2e2f71b0843a989f1a48d34ad117c12dc682ec1
Vadivelravichndran <136533480+Vadivelravichndran@users.noreply.github.com>
2023-06-16 03:24:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,13 +30,13 @@ def get_fruityvice_data(this_fruit_choice):
 streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
+if not fruit_choice:
     streamlit.error("please select a fruit get information.")
-  else:
+else:
     back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
     
-streamlit.error()
+
 streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
