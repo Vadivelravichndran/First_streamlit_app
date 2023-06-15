e7802d2077c8d3d178de8813fda8c86812a35e0a
Vadivelravichndran <136533480+Vadivelravichndran@users.noreply.github.com>
2023-06-16 00:40:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,7 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
 streamlit.text(fruityvice_response)
 
+Import snowflake.connector
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 streamlit.dataframe(fruityvice_normalized)
