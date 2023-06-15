5530b16e963b6ce4a81d3b1d408e44ace05c9f8a
Vadivelravichndran <136533480+Vadivelravichndran@users.noreply.github.com>
2023-06-16 02:43:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,13 +30,12 @@ try:
   if not fruit_choice:
     streamlit.error("please select a fruit get information.")
   else:
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  streamlit.dataframe(fruityvice_normalized)
-streamlit.write('The user entered ', fruit_choice)
-
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+    streamlit.write('The user entered ', fruit_choice)
 except url error as e:
-streamlit.error()
+  streamlit.error()
 
 
 streamlit.stop()
