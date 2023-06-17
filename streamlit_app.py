bd20bf49dc08c41d85c3dcc837e86c50647342f7
Vadivelravichndran <136533480+Vadivelravichndran@users.noreply.github.com>
2023-06-17 12:55:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,6 @@ if streamlit.button ('Get Fruit load list'):
   my_data_rows = get_fruit_load_list()
   my_cnx.close ()
   streamlit.dataframe(my_data_rows)
-  
   streamlit.header("What fruit would you like information about:")
   def insert_row_snowflake(new_fruit):
     with my_cnx.cursor()as my_cur:
